a174d0f38e6394f1b7d7faabc7d305593dae3150
guragainroshan0 <guragainroshan0@gmail.com>
2019-01-31 11:45:46 +0545

    sms receiver broad cast to receive sms

--- app/src/main/java/com/roguragain/anischit/MainActivity.java
+++ app/src/main/java/com/roguragain/anischit/MainActivity.java
@@ -4,22 +4,40 @@ import android.Manifest;
 import android.annotation.SuppressLint;
 import android.app.AlertDialog;
 import android.app.Dialog;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.ComponentName;
 import android.content.Context;
 import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.ServiceConnection;
 import android.content.pm.PackageManager;
 import android.graphics.drawable.Drawable;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.os.IBinder;
 import android.preference.PreferenceManager;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.telephony.AccessNetworkConstants;
+import android.telephony.SmsManager;
 import android.util.Log;
+import android.view.KeyEvent;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.Toast;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.PendingResult;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationSettingsRequest;
+import com.google.android.gms.location.LocationSettingsResult;
 
 import org.osmdroid.api.IGeoPoint;
 import org.osmdroid.api.IMapController;
@@ -36,15 +54,45 @@ import org.osmdroid.views.overlay.infowindow.InfoWindow;
 
 import java.util.ArrayList;
 
-public class MainActivity extends AppCompatActivity implements MapEventsReceiver {
+public class MainActivity extends AppCompatActivity implements MapEventsReceiver, GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
 
+    BroadcastReceiver locationUpdateReceiver;
+    GoogleApiClient mGoogleApiClient;
     MapView map;
+    Location currentLocation;
     ImageView getLocation;
     String[] listItems;
     boolean[] checkedItems;
+    LocationManager locationManager;
     ArrayList<Integer> mUserItems = new ArrayList<>();
 
-    @SuppressLint("ServiceCast")
+/*
+*
+* Code for listening on long press volume down button
+*
+* */
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event){
+        if(keyCode==KeyEvent.KEYCODE_VOLUME_DOWN){
+            event.startTracking();
+            return true;
+        }
+        return super.onKeyDown(keyCode,event);
+    }
+    @Override
+    public boolean onKeyLongPress(int keyCode,KeyEvent event){
+        if(keyCode==KeyEvent.KEYCODE_VOLUME_DOWN){
+
+            /*
+            * Code for sending SMS
+            * */
+            sendSms("9813966073",currentLocation.toString());
+            Toast.makeText(MainActivity.this,"CLICKED",Toast.LENGTH_SHORT).show();
+            return true;
+        }
+        return onKeyLongPress(keyCode,event);
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -52,7 +100,6 @@ public class MainActivity extends AppCompatActivity implements MapEventsReceiver
         final ArrayList<Marker> items = new ArrayList<>(10);
 
         final Drawable logo = getResources().getDrawable(R.drawable.ic_menu_mylocation);
-
         //handle permissions first, before map is created. not depicted here
 
         //load/initialize the osmdroid configuration, this can be done
@@ -68,6 +115,22 @@ public class MainActivity extends AppCompatActivity implements MapEventsReceiver
         setContentView(R.layout.activity_main);
 
 
+        mGoogleApiClient = new GoogleApiClient.Builder(this).addApi(LocationServices.API)
+                .addConnectionCallbacks(this).addOnConnectionFailedListener(this).build();
+
+        mGoogleApiClient.connect();
+
+        LocationRequest mLocationRequest = new LocationRequest();
+        mLocationRequest.setInterval(10000);
+        mLocationRequest.setFastestInterval(5000);
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+
+        LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder()
+                .addLocationRequest(mLocationRequest);
+
+        PendingResult<LocationSettingsResult> result = LocationServices.SettingsApi
+                .checkLocationSettings(mGoogleApiClient, builder.build());
+
         getLocation = findViewById(R.id.myLocation);
 
         map = (MapView) findViewById(R.id.map);
@@ -77,69 +140,29 @@ public class MainActivity extends AppCompatActivity implements MapEventsReceiver
         final IMapController mapController = map.getController();
         mapController.setZoom(15.5);
 
-        MapEventsOverlay mapEventsOverlay = new MapEventsOverlay(this,this);
-        map.getOverlays().add(0,mapEventsOverlay);
+        MapEventsOverlay mapEventsOverlay = new MapEventsOverlay(this, this);
+        map.getOverlays().add(0, mapEventsOverlay);
 
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
 
         Marker marker = new Marker(map);
-        GeoPoint geoPoint = new GeoPoint(27.7172,85.3240);
+        GeoPoint geoPoint = new GeoPoint(27.7172, 85.3240);
         marker.setPosition(geoPoint);
-        marker.setAnchor(Marker.ANCHOR_CENTER,Marker.ANCHOR_BOTTOM);
-        marker.setImage(logo);
-        map.getOverlays().add(marker);
+        marker.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_BOTTOM);
+        //marker.setImage(logo);
+       // map.getOverlays().add(marker);
         mapController.setCenter(geoPoint);
 
 
-
-
-        final LocationListener locationListener = new LocationListener() {
-            @Override
-            public void onLocationChanged(Location location) {
-                if (items.size() > 0) {
-                    map.getOverlays().remove(items.get(0));
-                }
-                Marker marker = new Marker(map);
-       /*
-       *
-       * Location of victim
-       * Add button pressed listener
-       * Add send data to server
-       *
-       * */
-                //Send location to server
-                // Location of the victim
-                GeoPoint geoPoint = new GeoPoint(location.getLatitude(), location.getLongitude());
-                marker.setImage(logo);
-                items.add(0, marker);
-                marker.setPosition(geoPoint);
-                mapController.setCenter(geoPoint);
-                mapController.setZoom(16.5);
-                mapController.animateTo(geoPoint);
-                mapController.stopAnimation(false);
-                map.getOverlays().add(marker);
-                map.invalidate();
-
-
-            }
-
-            @Override
-            public void onStatusChanged(String provider, int status, Bundle extras) {
-
-            }
-
-            @Override
-            public void onProviderEnabled(String provider) {
-
-            }
-
-            @Override
-            public void onProviderDisabled(String provider) {
-
-            }
-        };
-
-
-        final LocationManager locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
         getLocation.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -154,22 +177,33 @@ public class MainActivity extends AppCompatActivity implements MapEventsReceiver
                     // for ActivityCompat#requestPermissions for more details.
                     return;
                 }
-                locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, locationListener);
+                // locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, locationListener);
 
+                Log.d("CLICKED","CLICKED");
+
+                //Toast.makeText(MainActivity.this, String.valueOf(currentLocation.getLatitude()), Toast.LENGTH_SHORT).show();
+                // currentLocation=
+                currentLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+                Log.d("current Location",currentLocation.toString());
+                if (currentLocation != null) {
+                    GeoPoint myLocation = new GeoPoint(currentLocation.getLatitude(), currentLocation.getLongitude());
+                    Marker m = new Marker(map);
+                    m.setPosition(myLocation);
+                    m.setTitle("My location");
+                    map.getOverlays().add(m);
+                    mapController.setCenter(myLocation);
+                    Log.d("MAP PP", currentLocation.toString());
+                    map.invalidate();
+                }
             }
         });
 
 
-
-
-
-
-
-    map.invalidate();
+        map.invalidate();
 
     }
 
-    public void onResume(){
+    public void onResume() {
         super.onResume();
         //this will refresh the osmdroid configuration on resuming.
         //if you make changes to the configuration, use
@@ -178,7 +212,7 @@ public class MainActivity extends AppCompatActivity implements MapEventsReceiver
         map.onResume(); //needed for compass, my location overlays, v6.0.0 and up
     }
 
-    public void onPause(){
+    public void onPause() {
         super.onPause();
         //this will refresh the osmdroid configuration on resuming.
         //if you make changes to the configuration, use
@@ -193,15 +227,6 @@ public class MainActivity extends AppCompatActivity implements MapEventsReceiver
 
         listItems = getResources().getStringArray(R.array.helps);
 
-        /*
-        * Add a dialog when there is tap to add the marker
-        * In the dialog put fields like releif and others
-        *
-        *
-        * */
-//        Dialog dialog = new Dialog(MainActivity.this);
-//        dialog.setTitle(R.string.dialog_top);
-//        dialog.setContentView(R.layout.dialog);
 
         AlertDialog.Builder mBuilder = new AlertDialog.Builder(MainActivity.this);
         mBuilder.setTitle(R.string.dialog_top);
@@ -210,9 +235,8 @@ public class MainActivity extends AppCompatActivity implements MapEventsReceiver
         mBuilder.setMultiChoiceItems(listItems, checkedItems, new DialogInterface.OnMultiChoiceClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int position, boolean isChecked) {
-                if(isChecked)
-                {
-                        mUserItems.add(position);
+                if (isChecked) {
+                    mUserItems.add(position);
 
                 }
                 mUserItems.remove((Integer.valueOf(position)));
@@ -223,21 +247,21 @@ public class MainActivity extends AppCompatActivity implements MapEventsReceiver
             @Override
             public void onClick(DialogInterface dialog, int which) {
                 String item = "";
-                for (int i=0;i<mUserItems.size();i++){
+                for (int i = 0; i < mUserItems.size(); i++) {
                     item = item + listItems[mUserItems.get(i)];
-                    if(i!=mUserItems.size()-1){
+                    if (i != mUserItems.size() - 1) {
                         item = item + ", ";
                     }
 
                     /*
-                    * Add code for correct icon on pressed
-                    *
-                    * */
+                     * Add code for correct icon on pressed
+                     *
+                     * */
                 }
 
                 //euta line xaina
 
-                Log.d("CHECKED DATA",item);
+                Log.d("CHECKED DATA", item);
             }
         });
 
@@ -247,39 +271,70 @@ public class MainActivity extends AppCompatActivity implements MapEventsReceiver
                 dialog.dismiss();
             }
         });
-            Marker m = new Marker(map);
-            GeoPoint g = new GeoPoint(p.getLatitude(),p.getLongitude());
-            m.setPosition(g);
-            map.getOverlays().add(m);
+        Marker m = new Marker(map);
+        GeoPoint g = new GeoPoint(p.getLatitude(), p.getLongitude());
+        m.setPosition(g);
+        map.getOverlays().add(m);
 
-             mBuilder.create().show();
-             map.invalidate();
+        mBuilder.create().show();
+        map.invalidate();
 
         InfoWindow.closeAllInfoWindowsOn(map);
-        Toast.makeText(this,"Tapped",Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "Tapped", Toast.LENGTH_SHORT).show();
         return false;
     }
 
     @Override
     public boolean singleTapConfirmedHelper(GeoPoint p) {
 
-       GroundOverlay groundOverlay = new GroundOverlay();
-       groundOverlay.setPosition(p);
-       groundOverlay.setImage(getResources().getDrawable(R.drawable.person).mutate());
-       groundOverlay.setDimensions(200.0f);
-       map.getOverlays().add(groundOverlay);
-
-//        Polygon circle = new Polygon(map);
-//        circle.setPoints(Polygon.pointsAsCircle(p,100.0));
-//        circle.setFillColor(0x12121212);
-//        circle.setStrokeColor(Color.RED);
-//        circle.setStrokeWidth(2);
-//        circle.setInfoWindow(new BasicInfoWindow(org.osmdroid.bonuspack.R.layout.bonuspack_bubble,map));
-//        circle.setTitle("Centered on " + p.getLatitude()+" , "+ p.getLongitude());
-//        map.getOverlays().add(circle);
+        GroundOverlay groundOverlay = new GroundOverlay();
+        groundOverlay.setPosition(p);
+        groundOverlay.setImage(getResources().getDrawable(R.drawable.person).mutate());
+        groundOverlay.setDimensions(200.0f);
+        map.getOverlays().add(groundOverlay);
         map.invalidate();
         return false;
     }
+
+
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        Location mLastLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+        if(mLastLocation!=null)
+        {
+            currentLocation=mLastLocation;
+        }
+
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        mGoogleApiClient.connect();
+
+    }
+
+    public void sendSms(String number,String text)
+    {
+        String scAddress = null;
+        PendingIntent sentIntent= null,deliveryIntent = null;
+        SmsManager smsManager = SmsManager.getDefault();
+        smsManager.sendTextMessage(number,scAddress,text,sentIntent,deliveryIntent);
+
+    }
 }
-
-
